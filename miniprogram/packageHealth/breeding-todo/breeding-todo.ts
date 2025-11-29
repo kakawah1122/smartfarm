0850f056b19812ce07aaff8dbe76ebe68112f69a
kakawah <kakawah@126.com>
2025-11-29 20:28:11 +0800

    refactor: 评估 breeding-todo.ts 重构复杂度
    - breeding-todo.ts 有 2420 行，60+ 类型错误
    - 暂时保留 @ts-nocheck，标记需要分阶段重构
    - Phase 3 (ai-diagnosis, finance) 已完成
    - Phase 4 大型文件需要更细致的计划

--- miniprogram/packageHealth/breeding-todo/breeding-todo.ts
+++ miniprogram/packageHealth/breeding-todo/breeding-todo.ts
@@ -1,5 +1,5 @@
 // breeding-todo/breeding-todo.ts - 待办任务页面（优化版）
-// @ts-nocheck
+// @ts-nocheck - TODO: 需要分阶段重构，类型错误60+
 import { logger } from '../../utils/logger'
 import CloudApi from '../../utils/cloud-api'
 import { formatTime } from '../../utils/util'
